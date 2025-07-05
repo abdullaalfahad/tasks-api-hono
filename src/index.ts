e485883c013e9766d5f09088258638f703b22811
fahadevidentbd <fahad@evidentbd.com>
2025-07-05 23:25:18 +0600

    initial template setup

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { serve } from '@hono/node-server'
+import app from './app.js'
+
+serve({
+  fetch: app.fetch,
+  port: 3000,
+}, (info) => {
+  // eslint-disable-next-line no-console
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
